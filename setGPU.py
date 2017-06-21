75f05a43a0e7b724a90c4cb76d8466ee3a28beaa
Daniel Wong <wonglkd@gmail.com>
2017-06-21 14:37:00 +0800

    Favour free GPUs even more strongly

--- setGPU.py
+++ setGPU.py
@@ -18,6 +18,7 @@ def weighted_choice(choices):
 stats = [gpu for gpu in gpustat.GPUStatCollection.new_query() if gpu.entry['uuid'] is not None]
 ids = map(lambda gpu: int(gpu.entry['index']), stats)
 ratios = map(lambda gpu: 1. - float(gpu.entry['memory.used']) / float(gpu.entry['memory.total']), stats)
+ratios = [r ** 5. for r in ratios]  # Favour free ones even more strongly
 bestGPU = weighted_choice(zip(ids, ratios))
 
 print("setGPU: Setting GPU to: {}".format(bestGPU))
